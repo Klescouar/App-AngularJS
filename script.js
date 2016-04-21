ead062fd59b4b5563689d488155ce674ca221044
Poncho <lescouarneckevin@yahoo.fr>
2016-04-21 12:17:41 +0200

    Finish

--- script.js
+++ script.js
@@ -1,34 +1,81 @@
 var app = angular.module('appNote', []);
 
-app.controller('noteCtrl', function($scope) {
-	// body...
-	$scope.text = "";
-	$scope.memory = "";
-	$scope.status = 0;
+app.controller('noteCtrl', ["$scope", function($scope) {
+    $scope.elem = 1;
+    $scope.total = 300;
+    $scope.numPanier=0;
+      $scope.prixPanier = 0;
+    $scope.products = [{
+        name: 'Web Development',
+        price: 300,
+        active: true,
+        onoff : true,
+    }, {
+        name: 'Design',
+        price: 400,
+        active: false,
+        onoff : false,
 
-	$scope.$watch('text', function (newValue, oldValue) {
-    	if($scope.text.length > 100) {
-    		$scope.text = oldValue;
-    	}
-  	});
+    }, {
+        name: 'Integration',
+        price: 250,
+        active: false,
+        onoff : false,
 
-  	$scope.$watch('text', function (newValue, oldValue) {
-    	if($scope.text.length > 0) {
-    		$scope.memory = "Note modifiée";
-    		$scope.status = 1;
-    	}
-  	});
+    }, {
+        name: 'Formation',
+        price: 220,
+        active: false,
+        onoff : false,
 
-	$scope.erase = function(){
-		$scope.text='';
-		$scope.memory = "Note supprimée !";
-		$scope.status = 2;
-	}
+    }];
 
-	$scope.save = function(){
-		$scope.text='';
-		$scope.memory = "Note sauvegardée";
-		$scope.status = 3;
-	}
 
-});
+    $scope.addPrice = function() {
+        if (this.product.active == false) {
+            $scope.total += this.product.price;
+            this.product.active = true;
+            $scope.elem += 1;
+            this.product.onoff = true;
+        }
+        else if (this.product.active == true) {
+            $scope.total -= this.product.price;
+            this.product.active = false;
+            $scope.elem -= 1;
+            this.product.onoff = false;
+        }
+    }
+
+    $scope.addPanier = function(){
+  $scope.numPanier += $scope.elem;
+  $scope.prixPanier += $scope.total;
+  $scope.elem = 1;
+  $scope.total = 300;
+  $scope.products = [{
+      name: 'Web Development',
+      price: 300,
+      active: true,
+      onoff : true,
+  }, {
+      name: 'Design',
+      price: 400,
+      active: false,
+      onoff : false,
+
+  }, {
+      name: 'Integration',
+      price: 250,
+      active: false,
+      onoff : false,
+
+  }, {
+      name: 'Formation',
+      price: 220,
+      active: false,
+      onoff : false,
+
+  }];
+    }
+
+
+}]);
